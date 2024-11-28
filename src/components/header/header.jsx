44305dd3caf0d7d601c87413d06e0bcc4dc0df22
Oru <tejirioru@gmail.com>
2024-11-28 14:03:59 +0100

    trying to get appropiate positions for the header contents

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./header.css"
-// import logoImg from "../../images/cookaton.png";
+import logoImg from "../../images/cookaton.png";
 
 
 
@@ -8,18 +8,24 @@ import "./header.css"
 const header = () => {
   return (
     <div>
+        <div className='headings2'>
+        <div className='heading'>
+        <div className='logos'>
+            <img
+            src={logoImg}
+            width={120}
+            height={120}
+            className="cookingtons"
+            alt="Villa"
+        />
+        </div>
         <div className="headers">
-        {/* <img
-        src={logoImg}
-        width={80}
-        height={80}
-        className="cottage"
-        alt="Villa"
-      /> */}
             <h3>Home</h3>
             <h3>Recipe</h3>
             <h3>Cooking tips</h3>
             <h3>About us</h3>
+        </div>
+        </div>
         </div>
     </div>
   )
