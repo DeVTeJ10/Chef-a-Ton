67de38529694eb045544180e133499b02b37e15a
Oru <tejirioru@gmail.com>
2024-12-07 22:09:30 +0100

    adding more icons and pictures for the recipe page like the serving and time frame icons

--- src/pages/recipe/recipePage.jsx
+++ src/pages/recipe/recipePage.jsx
@@ -1,5 +1,7 @@
 import Header from "../../components/header/header"
-import pasta from "../../images/pasta.jpg";
+import streetfood from "../../images/streetfood.jpg";
+import perfectiming from "../../images/perfecttime.png"
+import foodserving from "../../images/foodserving.png"
 import "./recipe.css"
 
 
@@ -7,17 +9,43 @@ const recipesPage = () => {
   return (
     <div>
       <Header/>
-      <div className="foodingrecipe">
-          <h1>Tomato Pasta and Sauce</h1>
-      </div>
+      <div>
+          <h1 className="foodingrecipe">Street food varieties</h1>
 
-          <img
-                  src={pasta}
-                  width={600}
-                  height={400}
-                  className="cooking2ton"
+          <div className="timeperfect">
+          <div className="perfectime">
+        <img
+                  src={perfectiming}
+                  width={40}
+                  height={40}
                   alt="pasta"
+                  className="cooking2tons"
               />
+              <h3>1</h3>
+              <h3>Hour</h3>
+        </div>
+        <div className="perfectime">
+        <img
+                  src={foodserving}
+                  width={40}
+                  height={40}
+                  alt="pasta"
+                  className="cooking2tons"
+              />
+              <h3>4</h3>
+              <h3>Serves</h3>
+        </div>
+          </div>
+        <div className="cooking2ton">
+        <img
+                  src={streetfood}
+                  width={1400}
+                  height={500}
+                  alt="pasta"
+                  className="cooking2tons"
+              />
+        </div>
+      </div>
       </div>
   )
 }
