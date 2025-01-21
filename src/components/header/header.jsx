bfa8183dd6cf7e3f076792ad4e032e6ec827a91e
Oru <tejirioru@gmail.com>
2025-01-21 23:20:20 +0100

    still trying to get display name in the header for display

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,66 +1,65 @@
 import { Link } from "react-router-dom";
+import useUserName from "../../userNameHook";
 import { useParams } from "react-router-dom";
 import "./header.css"
 import logoImg from "../../images/cookaton.png";
+import { useState } from "react";
 
+const Header = () => {
+    const { user } = useUserName();
+    console.log(user);
 
+    return (
+        <div>
+            <div className='heading2'>
+                <div className='heading'>
+                    <div className='logos'>
+                        <img
+                            src={logoImg}
+                            width={80}
+                            height={80}
+                            className="cookingtons"
+                            alt="Villa"
+                        />
+                    </div>
 
+                    <div className="headersLinks">
+                        <div className="headers">
+                            <Link to={"/"} className="homeTag">
+                                <h3>Home</h3>
+                            </Link>
+                            {/* <Link to={"/recipe-page"} className="recipeTag"> */}
+                            <h3>Recipe</h3>
+                            {/* </Link> */}
+                            <Link to={"/aboutUs-page"} className="aboutUsTag">
+                                <h3>{user.username}</h3>
+                            </Link>
+                            <Link to={"/favourites-page"} className="favouritesTag">
+                                <h3>Favourites</h3>
+                            </Link>
+                        </div>
+                    </div>
 
-const header = () => {
-  return (
-    <div>
-        <div className='heading2'>
-        <div className='heading'>
-        <div className='logos'>
-            <img
-            src={logoImg}
-            width={80}
-            height={80}
-            className="cookingtons"
-            alt="Villa"
-        />
+                    <div className="inputinup">
+                        <input
+                            placeholder='Search for recipe' 
+                            className='inputRecipe'/>
+
+                        {
+                            <div className="authentication">
+                                <Link to={"/login"}>
+                                    <button className="login">Login</button>
+                                </Link>
+                                <Link to={"/signup"}>
+                                    <button className="signUp">Sign up</button>
+                                </Link>
+                            </div>
+                        }
+                    </div>
+                </div>
+            </div>
         </div>
-
-        <div className="headersLinks">
-        <div className="headers">
-          <Link to={"/"} className="homeTag">
-              <h3>Home</h3>
-            </Link>
-          {/* <Link to={"/recipe-page"} className="recipeTag"> */}
-                <h3>Recipe</h3>
-            {/* </Link> */}
-          <Link to={"/aboutUs-page"} className="aboutUsTag">
-                <h3>About us</h3>
-              </Link>
-          <Link to={"/favourites-page"} className="favouritesTag">
-                <h3>Favourites</h3>
-              </Link>
-        </div>
-        </div>
-
-        <div className="inputinup">
-        <input
-        placeholder='Search for recipe' 
-        className='inputRecipe'/>
-
-
-        {
-
-          
-          <div className="authentication">
-          <Link to={"/login"}>
-            <button className="login">Login</button>
-          </Link>
-          <Link to={"/signup"}>
-            <button className="signUp">Sign up</button>
-          </Link>
-          </div>
-        }
-        </div>
-        </div>
-        </div>
-    </div>
-  )
+    )
 }
 
-export default header
+export default Header
