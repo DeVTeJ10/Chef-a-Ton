914250798dd312c09175d4e24eb5b8c60b28321c
Oru <tejirioru@gmail.com>
2025-07-18 16:30:46 +0100

    previous error was not fully resolved

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1500, // Increase the warning limit to 1.5MB
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendors': ['react', 'react-dom', 'react-router-dom'],
+        }
+      }
+    }
+  }
 })
